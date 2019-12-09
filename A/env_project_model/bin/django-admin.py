c7a6fc89d6f9321c6e97edfb719c0811ffd89433
Garry Ariel <garry@alterra.id>
2019-12-09 11:21:40 +0700

    Django Task

---
+++ A/env_project_model/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta8/Alterra_Academy/DJANGO_MVC/A/env_project_model/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ C/env_project_advance_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta8/Alterra_Academy/DJANGO_MVC/C/env_project_advance_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
