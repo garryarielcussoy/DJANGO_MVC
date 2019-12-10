723828309cc8156580ed14659f0ca0b74eafe8e2
Garry Ariel <garry@alterra.id>
2019-12-10 17:40:47 +0700

    Ready to Start the Project

--- C/project_advance_views/alphatech/models.py
+++ C/project_advance_views/alphatech/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    judul_post = models.CharField(max_length=255, default="")
+    waktu_publikasi = models.DateTimeField("Waktu Tayang")
+    jumlah_komentar = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.judul_post
+
+class Mentee(models.Model):
+    nama_lengkap = models.CharField(max_length=255, default="")
+    foto = models.ImageField()
+    testimoni = models.TextField(default="")
+
+    def __str__(self):
+        return self.nama_lengkap
+
+class Mentor(models.Model):
+    nama_lengkap = models.CharField(max_length=255, default="")
+    foto = models.ImageField()
+    testimoni = models.TextField(default="")
+    pengalaman = models.TextField(default="")
+
+    def __str__(self):
