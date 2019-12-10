a5f3d3c8907506c33137978843abb1d1303aa221
Garry Ariel <garry@alterra.id>
2019-12-10 20:14:48 +0700

    Model Revision

--- C/project_advance_views/alphatech/models.py
+++ C/project_advance_views/alphatech/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     foto = models.CharField(max_length=255, default="")
-    waktu_publikasi = models.DateTimeField("Waktu Tayang")
+    waktu_publikasi = models.DateField("Waktu Tayang")
     jumlah_komentar = models.IntegerField(default=0)
     judul_post = models.CharField(max_length=255, default="")
     konten = models.TextField(default="")
